d0451df9a19c3a709daea3161988da4bbc36a941
Augusto Pimenta  (6023) <apimenta@luxfacta.com.br>
2017-10-09 12:43:36 -0300

    Removendo informacoes de teste

---
+++ src/default.env.js
@@ -0,0 +1,3 @@
+export const LOGIN_URL = 'http://domain.com/login';
+
+export const LOGIN_PARAMS = {};
--- src/services/request.js
+++ src/services/request.js
@@ -1,16 +1,6 @@
 import axios from 'axios';
+import * as env  from '../env';
 
 export const requestLogin = id => {
-    const data = {
-        '_token': '',
-        'isTablet': 0,
-        'impressora': 0,
-        'usuarioAD': '99705636',
-        'numeroUsuario': id,
-        'senhaAD': '',
-        'cpf': '024.252.377-39',
-        'nascimento': '13/08/1976'
-    };
-
-    return axios.post('http://portalclick-aceite.la.interbrew.net/login', data);
+    return axios.post(env.LOGIN_URL, { ...env.LOGIN_PARAMS, numeroUsuario: id });
 };
